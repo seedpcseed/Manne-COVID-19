535e304fad03ab23621a4d48946e3d50e2efae8f
seedpcseed <seedpcseed@gmail.com>
2020-04-13 04:57:04 -0500

    added data input

---
+++ COVID-19-Survey-Analysis.R
@@ -0,0 +1,28 @@
+---
+title: "Manne Research Institute COVID-19 Expertise and Investigations"
+author: "Patrick Seed"
+date: "April 12, 2020"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+```{r libraries message=FALSE, error=FALSE, warning=FALSE}
+library(tidyverse)
+library(ggplot2)
+library(readxl)
+library(highcharter)
+```
+
+
+```{r read survey data message=FALSE, error=FALSE, warning=FALSE}
+covid.df<- read_xlsx("./data/Manne Research Institute COVID-19 Research Survey (1).xlsx")
+```
+
+
+```{r }
+
+
+```
